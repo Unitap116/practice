416735a03a5589f2ff17e5f497e22bdc53e09cf7
Marina Labenskaya <marinalabenskaya@lxque-mba.headscale.cmld.network>
2025-07-15 16:20:13 +0300

    feat: add @Valid

--- profile/src/main/java/ru/unitap/profile/controller/ProfileController.java
+++ profile/src/main/java/ru/unitap/profile/controller/ProfileController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -75,7 +76,7 @@ public class ProfileController {
   @PostMapping("/{id}/avatar")
   public ProfileResponseDto uploadAvatar(
     @PathVariable("id") UUID id,
-    @RequestBody AvatarUploadRequestDto avatarUploadRequestDto) {
+    @RequestBody @Valid AvatarUploadRequestDto avatarUploadRequestDto) {
     return profileService.uploadAvatar(id, avatarUploadRequestDto);
   }
 }
