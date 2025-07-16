54b90d55359cf0f3971e9ddd7df2f2376e8c3f47
Marina Labenskaya <marinalabenskaya@MacBook-Air-Marina.local>
2025-07-16 12:09:19 +0300

    feat: create new project with maven

--- unitap-profile/src/main/java/ru/unitap/profile/controller/ProfileController.java
+++ unitap-profile/src/main/java/ru/unitap/profile/controller/ProfileController.java
@@ -14,8 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.unitap.profile.dto.request.AvatarUploadRequestDto;
-import ru.unitap.profile.dto.response.ProfileResponseDto;
 import ru.unitap.profile.dto.request.ProfileUpdateRequestDto;
+import ru.unitap.profile.dto.response.ProfileResponseDto;
 import ru.unitap.profile.service.ProfileService;
 
 import java.util.UUID;
--- unitap-profile/src/main/java/ru/unitap/profile/mapper/ProfileMapper.java
+++ unitap-profile/src/main/java/ru/unitap/profile/mapper/ProfileMapper.java
@@ -4,7 +4,7 @@ import org.mapstruct.Mapper;
 import ru.unitap.profile.dto.response.ProfileResponseDto;
 import ru.unitap.profile.entity.ProfileEntity;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface ProfileMapper {
 
   ProfileResponseDto toResponse(ProfileEntity profileEntity);
