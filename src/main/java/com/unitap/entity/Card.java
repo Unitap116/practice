e0e3324a3faa822ceadfce6df74c52385288a0d4
Alex Kirilin <kirilin.75.ru@gmail.com>
2025-07-16 11:17:45 +0300

    convert from gradle to maven

--- src/main/java/com/unitap/entity/Card.java
+++ src/main/java/com/unitap/entity/Card.java
@@ -1,8 +1,17 @@
 package com.unitap.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Card {
     private UUID cardId;
-    String name;
+    private String name;
+    private Boolean isPublic;
+    private Long views;
 }
