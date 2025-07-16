8474fbfe0d54082e504b3f8a415fc9c1c67be9e7
Unitap116 <unitap116@gmail.com>
2025-07-16 22:46:00 +0300

    Revert "[Draft] feat: profile"

--- profile/src/main/java/ru/unitap/profile/exception/model/ProfileNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.unitap.profile.exception.model;
-
-public class ProfileNotFoundException extends NotFoundException {
-
-  public ProfileNotFoundException() {
-    super("exception.profile.not-found");
-  }
-}
